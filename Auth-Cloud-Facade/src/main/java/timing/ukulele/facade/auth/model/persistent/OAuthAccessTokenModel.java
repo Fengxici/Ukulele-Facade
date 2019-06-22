d81c7faca970fc63511d948918f7b00c7376dc7d
910980941@qq.com <910980941@qq.com>
2019-06-22 09:45:30 +0800

    误删

---
+++ Auth-Cloud-Facade/src/main/java/timing/ukulele/facade/auth/model/persistent/OAuthAccessTokenModel.java
@@ -0,0 +1,17 @@
+package timing.ukulele.facade.auth.model.persistent;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+
+@Data
+@TableName("oauth_access_token")
+public class OAuthAccessTokenModel {
+    private String tokenId;
+    private byte[] token;
+    private String authenticationId;
+    private String userName;
+    private String clientId;
+    private String authentication;
+    private String refreshToken;
+}
